4a85d0bb6fcd8c927956b5d77c632af899c4f1f6
kelimacaxia <39752386+kelimacaxia@users.noreply.github.com>
2018-05-30 10:37:25 +0800

    Add files via upload
    Upload source code and graphic

---
+++ cae-mnist.py
@@ -0,0 +1,122 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# import math
+# import random
+
+import tensorflow as tf
+import tensorflow.examples.tutorials.mnist.input_data as input_data
+
+from tensorflow.python.framework import ops
+from tensorflow.python.framework import graph_util
+
+from argparse import ArgumentParser
+
+import model as model
+import warnings
+import os
+
+warnings.filterwarnings("ignore")
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+ops.reset_default_graph()
+
+mnist = input_data.read_data_sets("data/", one_hot=True)
+
+
+def train(options):
+    batch_size = options.batch_size
+    n_epochs = options.n_epochs
+
+    trainimgs = mnist.train.images
+
+    print("Network ready")
+    learning_rate = 0.001
+
+    dim = trainimgs.shape[1]
+    x = tf.placeholder(tf.float32, [None, dim], name='x')
+    # x = tf.placeholder(tf.float32, [None, 28, 28, 1], name='x')
+    y = tf.placeholder(tf.float32, [None, dim])
+    keepprob = tf.placeholder(tf.float32, name='keepprob')
+
+    pred = model.cnn(x,  keepprob)  # ['out']
+    cost = tf.reduce_sum(tf.square(pred - tf.reshape(y, shape=[-1, 28, 28, 1])), name='cost')
+    optm = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+    out_img = tf.reshape(pred, [28*28], name="out_img")
+
+    print("Start training..")
+
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        print(x.get_shape())
+        sess.run(init)
+        for epoch_i in range(n_epochs):
+            for batch_i in range(mnist.train.num_examples // batch_size):
+                batch_xs, _ = mnist.train.next_batch(batch_size)
+                trainbatch_noisy = batch_xs + 0.3 * np.random.randn(batch_xs.shape[0], 784)
+                sess.run(optm, feed_dict={x: trainbatch_noisy, y: batch_xs, keepprob: 0.7})
+                print("[%02d/%02d] cost: %.4f" % (epoch_i, n_epochs, sess.run(cost, feed_dict={x: trainbatch_noisy, y: batch_xs, keepprob: 1.})))
+
+        print(out_img)
+        # print(recon)
+
+        print("Save model")
+        graph_def = tf.get_default_graph().as_graph_def()
+        output_graph = graph_util.convert_variables_to_constants(sess, graph_def, ['out_img'])
+        with tf.gfile.GFile(os.path.join('./', 'mnist' + '.pb'), 'wb') as f:
+            f.write(output_graph.SerializeToString())
+
+
+def test():
+    testimgs = mnist.test.images
+
+    with tf.gfile.GFile(os.path.join('./', 'mnist' + '.pb'), "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.graph.as_default()
+        tf.import_graph_def(graph_def, name='')
+
+        sess.run(init)
+
+        x_ = sess.graph.get_tensor_by_name('x:0')
+        out_ = sess.graph.get_tensor_by_name('out_img:0')
+        keepprob_ = sess.graph.get_tensor_by_name('keepprob:0')
+
+        n_examples = 1
+        test_xs, _ = mnist.test.next_batch(n_examples)
+        test_xs_noisy = test_xs + 0.3 * np.random.randn(test_xs.shape[0], 784)
+        recon = sess.run(out_, feed_dict={x_: test_xs_noisy, keepprob_: 1.})
+        fig, axs = plt.subplots(3, n_examples, figsize=(15, 4))
+
+        axs[0].matshow(np.reshape(test_xs, (28, 28)), cmap=plt.get_cmap('gray'))
+        axs[1].matshow(np.reshape(test_xs_noisy, (28, 28)), cmap=plt.get_cmap('gray'))
+        axs[2].matshow(np.reshape(np.reshape(recon, (784,)), (28, 28)), cmap=plt.get_cmap('gray'))
+        plt.show()
+    print('test')
+
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--cmd', type=str, dest='cmd', default='train', help='command')
+    parser.add_argument('--n_epochs', type=int, dest='n_epochs', default=10,
+                        help='training n_epochs')
+    parser.add_argument('--batch_size', type=int, dest='batch_size', default=128,
+                        help='batch size')
+    return parser
+
+
+def main():
+    parser = build_parser()
+    options = parser.parse_args()
+    print(options.cmd)
+    if options.cmd == 'train':
+        train(options)
+    elif options.cmd == 'test':
+        test()
+
+
+if __name__ == '__main__':
+    main()
---
+++ model.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+
+# WEIGHT AND BIASES
+n1 = 16
+n2 = 32
+n3 = 64
+ksize = 5
+
+def get_weight(shape):
+    return tf.get_variable("weight", shape, initializer=tf.truncated_normal_initializer(stddev=0.1))
+
+
+def get_bias(shape):
+    return tf.get_variable("bias", shape, initializer=tf.constant_initializer(0.0))
+
+
+def cnn(_X, _keepprob):
+    _input_r = tf.reshape(_X, shape=[-1, 28, 28, 1])
+    # Encoder
+    with tf.variable_scope("ce1"):
+        _ce1_weight = get_weight([ksize, ksize, 1, n1])
+        _ce1_bias = get_bias([n1])
+        _ce1 = tf.nn.sigmoid(tf.add(tf.nn.conv2d(_input_r, _ce1_weight, strides=[1, 2, 2, 1], padding='SAME'), _ce1_bias))
+        _ce1 = tf.nn.dropout(_ce1, _keepprob)
+
+    with tf.variable_scope("ce2"):
+        _ce2_weight = get_weight([ksize, ksize, n1, n2])
+        _ce2_bias = get_bias([n2])
+        _ce2 = tf.nn.sigmoid(tf.add(tf.nn.conv2d(_ce1, _ce2_weight, strides=[1, 2, 2, 1], padding='SAME'), _ce2_bias))
+        _ce2 = tf.nn.dropout(_ce2, _keepprob)
+
+    with tf.variable_scope("ce3"):
+        _ce3_weight = get_weight([ksize, ksize, n2, n3])
+        _ce3_bias = get_bias([n3])
+        _ce3 = tf.nn.sigmoid(tf.add(tf.nn.conv2d(_ce2, _ce3_weight, strides=[1, 2, 2, 1], padding='SAME'), _ce3_bias))
+        _ce3 = tf.nn.dropout(_ce3, _keepprob)
+
+    # Decoder
+    with tf.variable_scope("cd3"):
+        _cd3_weight = get_weight([ksize, ksize, n2, n3])
+        _cd3_bias = get_bias([n2])
+        _cd3 = tf.nn.sigmoid(tf.add(tf.nn.conv2d_transpose(_ce3, _cd3_weight, tf.stack([tf.shape(_X)[0], 7, 7, n2]), strides=[1, 2, 2, 1], padding='SAME'), _cd3_bias))
+        _cd3 = tf.nn.dropout(_cd3, _keepprob)
+
+    with tf.variable_scope("cd2"):
+        _cd2_weight = get_weight([ksize, ksize, n1, n2])
+        _cd2_bias = get_bias([n1])
+        _cd2 = tf.nn.sigmoid(tf.add(tf.nn.conv2d_transpose(_cd3, _cd2_weight, tf.stack([tf.shape(_X)[0], 14, 14, n1]), strides=[1, 2, 2, 1], padding='SAME'),_cd2_bias))
+        _cd2 = tf.nn.dropout(_cd2, _keepprob)
+
+    with tf.variable_scope("cd1"):
+        _cd1_weight = get_weight([ksize, ksize, 1, n1])
+        _cd1_bias = get_bias([1])
+        _cd1 = tf.nn.sigmoid(tf.add(tf.nn.conv2d_transpose(_cd2, _cd1_weight, tf.stack([tf.shape(_X)[0], 28, 28, 1]), strides=[1, 2, 2, 1], padding='SAME'), _cd1_bias))
+        _out = tf.nn.dropout(_cd1, _keepprob, name='output')
+
+    return _out
